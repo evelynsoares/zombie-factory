30f7a89a079e2b6a335475b9d401150e85960ab8
Evelyn Soares <65402409+evelynsoares@users.noreply.github.com>
2025-07-24 16:11:49 -0300

    removes a comment

--- Contract.sol
+++ Contract.sol
@@ -23,13 +23,3 @@ contract ZombieFactory {
 
     }
 }
-
-/*
-he first line of code should take the keccak256 hash of abi.encodePacked(_str) 
-to generate a pseudo-random hexadecimal,
- typecast it as a uint, and finally store the result in a uint called rand.
-
-We want our DNA to only be 16 digits long (remember our dnaModulus?). 
-So the second line of code should return the above value modulus (%) dnaModulus.
-
-*/
